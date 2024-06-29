6dc2d6112b69374293e96342defe5b361ea2af5e
Antonio Berbece <razvan.berbece@hotmail.com>
2024-06-29 16:50:44 +0100

    adds runtime configuration population at startup

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/org/go_worker/src/config"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Printf("Running in < %s >\n", config.Environment)
+	fmt.Printf("With DB connection string: < %s >\n", config.MySqlConnectionString)
 }
